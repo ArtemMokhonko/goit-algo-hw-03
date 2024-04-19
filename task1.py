523fc2a22fe9ff9eba146382cf89bd14861b3973
ArtemMokhonko <artem.stayl@gmail.com>
2024-04-19 15:17:58 +0300

    2

---
+++ task1.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+
+def get_days_from_today(date):
+    try:
+        date_now = datetime.today()
+        date = datetime.strptime(date, "%Y-%m-%d")
+        days_difference = date_now.toordinal()- date.toordinal()
+        return  days_difference
+    except ValueError:
+        return  "Wrong data format"
+
+
+res = get_days_from_today("203-12-21")
+print (res)
+
+
